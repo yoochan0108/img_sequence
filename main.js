34fa44425ec682bcedfbff3f6b20894da3abf55b
yoochan0108 <yoochan0108@naver.com>
2023-09-21 16:20:11 +0900

    tranition값 밀리세컨드로 변환

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const section = document.querySelector('section');
 const aside = document.querySelector('aside');
 const loadingNum = document.querySelector('aside p span');
 const imgs = createImgs(section, num);
+const delay = convertSpeed(aside);
 
 window.addEventListener('mousemove', (e) => {
 	const percent = getPercent(e, num);
@@ -34,11 +35,13 @@ function createImgs(target, num) {
 			count++;
 			const percent = parseInt((count / num) * 100);
 			loadingNum.innerText = percent;
-			console.log('현재 로딩된 소스이미지', count);
 			if (count === num) {
 				//동적으로 만들어진 img요소의 소스이미지가 렌더링완료된 시점
 				console.log('모든 소스이미지 로딩 완료');
-				aside.remove();
+				aside.classList.add('off');
+				setTimeout(() => {
+					aside.remove();
+				}, delay);
 			}
 		};
 	});
@@ -48,4 +51,11 @@ function createImgs(target, num) {
 function activation(arr, index) {
 	arr.forEach((el) => (el.style.display = 'none'));
 	arr[index].style.display = 'block';
+}
+//인수로 transition-duration값을 구해야되는 DOM요소를 전달받음
+function convertSpeed(el) {
+	//해당요소의 transition-duration값을 재연산해서 가져온다음
+	//숫자로 바꾸고 *1000을 해서 밀리세컨드 형태로 변환
+	const result = getComputedStyle(el).transitionDuration;
+	return parseFloat(result) * 1000;
 }
