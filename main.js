c7da567bafa4947785375fb0f05577675d35655b
yoochan0108 <yoochan0108@naver.com>
2023-09-21 14:11:12 +0900

    현재 이미지 수 구하기

--- main.js
+++ main.js
@@ -10,6 +10,10 @@
 //위의 100번 반복을 돌리면서
 //append로 이미지 요소 반복추가
 
+//백분율 구하는 공식
+//현재수치값 / 전체수치값 * 100 (백분율)
+//현재수치값 / 전체수치값 * 200 (이백분율)
+
 const section = document.querySelector('section');
 
 for (let i = 0; i <= 200; i++) {
@@ -19,3 +23,12 @@ for (let i = 0; i <= 200; i++) {
 	img.setAttributeNode(src);
 	section.append(img);
 }
+
+window.addEventListener('mousemove', (e) => {
+	const curPos = e.pageX;
+	const wid = window.innerWidth;
+	const percent = parseInt((curPos / wid) * 200);
+	console.log(percent);
+	//parseInt(숫자) : 실수에서 소수점 아래를 버려서 정수반환
+	//parseFloat(숫자) : 소수점 아래까지 있는 실수반환
+});
