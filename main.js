717a212a1032db662cbe5403139f919b88d49938
yoochan0108 <yoochan0108@naver.com>
2023-09-21 15:15:40 +0900

    이미지소스 로딩 이벤트 확인

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-//1.이미지 동적으로 200개 생성
-//2.이미지 소스가 로딩이 될때가 에러가 발생하는 시스템 이벤트 설정
-//3.브라우저에서 마우스 움직일때마다 마우스 좌표값 구하기
-//4.특정 수치값을 백분율화 하는 로직처리
-//5.이미지소스가 모두 로딩되는 상태를 백분율로 변환
-//img 노드 생성
-//src 속성 생성
-//src속성 노드에 value = img/pic0~100.jpg
-//위의 img돔생성을 100번 반복을 돌리면서
-//append로 section 프레임안에 추가
-//백분율 구하는 공식
-//현재수치값 / 전체수치값 * 100 (백분율)
-//현재수치값 / 전체수치값 * 200 (이백분율)
 //현재수치값 / 전체수치값 * 100 (백분율)
 //현재수치값 / 전체수치값 * 200 (이백분율)
 
@@ -18,23 +5,17 @@ const num = 200;
 const section = document.querySelector('section');
 const imgs = createImgs(section, num);
 
-//activation함수 추가 : 인수로 유사배열, 활성화 순번받음
-//순번에 대한 요소만 보임처리
-
 window.addEventListener('mousemove', (e) => {
 	const percent = getPercent(e, num);
 	activation(imgs, percent);
 });
 
-//이벤트정보 객체와 전체 개수를 받아서
-//해당 숫자에 대한 백분율 반환함수
 function getPercent(e, num) {
 	const curPos = e.pageX;
 	const wid = window.innerWidth;
 	return parseInt((curPos / wid) * num);
 }
 
-//인수로 갯수를 받아서 동적으로 img생성해 주는 함수
 function createImgs(target, num) {
 	for (let i = 0; i < num; i++) {
 		const img = document.createElement('img');
@@ -43,11 +24,22 @@ function createImgs(target, num) {
 		img.setAttributeNode(src);
 		target.append(img);
 	}
-	return target.querySelectorAll('img');
+	const imgs = target.querySelectorAll('img');
+	let count = 0;
+	imgs.forEach((img) => {
+		//해당 돔에 수반되는 소스이미지가 로딩완료시 실행되는 이벤트
+		img.onload = () => {
+			count++;
+			console.log('현재 로딩된 소스이미지', count);
+			if (count === num) {
+				//동적으로 만들어진 img요소의 소스이미지가 렌더링완료된 시점
+				console.log('모든 소스이미지 로딩 완료');
+			}
+		};
+	});
+	return imgs;
 }
 
-//인수로 그룹유사배열, 활성화 순번을 받아서
-//해당 순번의 요소만 활성화처리
 function activation(arr, index) {
 	arr.forEach((el) => (el.style.display = 'none'));
 	arr[index].style.display = 'block';
